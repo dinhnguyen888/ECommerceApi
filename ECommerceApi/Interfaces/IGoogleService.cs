436ecbf562afed57b6af363a621e3507a8a22f91
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-02-20 18:42:30 +0700

    chore: missing google Interface when push code

---
+++ ECommerceApi/Interfaces/IGoogleService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerceApi.Dtos;
+
+namespace ECommerceApi.Interfaces
+{
+    public interface IGoogleService
+    {
+        Task<string> GenerateTokenFromGoogleInfo(GoogleRequestDto googleRequestDto);
+    }
+}
