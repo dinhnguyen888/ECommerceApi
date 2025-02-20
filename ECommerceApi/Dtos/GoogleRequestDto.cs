261318b45e0e573222364382411cb1d535221597
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-02-20 18:38:10 +0700

    chore: missing google dto when push code to git

---
+++ ECommerceApi/Dtos/GoogleRequestDto.cs
@@ -0,0 +1,19 @@
+﻿namespace ECommerceApi.Dtos
+{
+    public class GoogleRequestDto
+    {
+        public string Iss { get; set; }
+        public string Sup { get; set; }
+        public string Azp { get; set; }
+        public string Aud { get; set; }
+        public string Iat { get; set; }
+        public string Exp { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Local { get; set; }
+        public string Picture { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string EmailVerified { get; set; }
+    }
+}
