617a0523570037bf3084e479c3f6f148573c712b
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-03-02 22:34:06 +0700

    chore: add status field for PaymentViewHistoryDto

--- ECommerceApi/Dtos/PaymentViewHistoryDto.cs
+++ ECommerceApi/Dtos/PaymentViewHistoryDto.cs
@@ -6,5 +6,6 @@
         public string PaymentGateway { get; set; }
         public long ProductPrice { get; set; }
         public string PaymentDate { get; set; }
+        public string PaymentStatus { get; set; }
     }
 }
