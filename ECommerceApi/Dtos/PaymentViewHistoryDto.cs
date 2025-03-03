1357cfec2b25392d80298118b8ccce547fc4b430
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-03-03 10:12:30 +0700

    fix: change string to bool in PaymentStatus

--- ECommerceApi/Dtos/PaymentViewHistoryDto.cs
+++ ECommerceApi/Dtos/PaymentViewHistoryDto.cs
@@ -6,6 +6,6 @@
         public string PaymentGateway { get; set; }
         public long ProductPrice { get; set; }
         public string PaymentDate { get; set; }
-        public string PaymentStatus { get; set; }
+        public bool PaymentStatus { get; set; }
     }
 }
