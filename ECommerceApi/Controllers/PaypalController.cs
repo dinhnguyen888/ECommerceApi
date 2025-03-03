3380f124eb43f8edce36ea774e26ac5503444bdb
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-03-03 10:23:54 +0700

    fix: add redirectURL for PAYPAL gateway

--- ECommerceApi/Controllers/PaypalController.cs
+++ ECommerceApi/Controllers/PaypalController.cs
@@ -12,10 +12,12 @@ namespace ECommerceApi.Controllers
     public class PayPalController : Controller
     {
         private readonly IPayPalService _payPalService;
+        private readonly IConfiguration _configuration;
 
-        public PayPalController(IPayPalService payPalService)
+        public PayPalController(IPayPalService payPalService, IConfiguration configuration)
         {
             _payPalService = payPalService;
+            _configuration = configuration;
         }
 
         [HttpPost("create-paypal-payment")]
@@ -39,7 +41,7 @@ namespace ECommerceApi.Controllers
             try
             {
                 await _payPalService.PaypalCallbackHandle(Request.Query);
-                return Ok();
+                return Redirect(_configuration["URL:FrontendUrlPaymentCallback"]);
             }
             catch (Exception ex)
             {
